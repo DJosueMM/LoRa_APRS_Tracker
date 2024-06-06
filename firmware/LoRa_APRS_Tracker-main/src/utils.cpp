b9c3a16599b8e0d178492a91c793bb2c44d60630
DJosueMM <djosuemm@gmail.com>
2024-06-06 09:48:25 -0600

    Readme

--- firmware/LoRa_APRS_Tracker-main/src/utils.cpp
+++ firmware/LoRa_APRS_Tracker-main/src/utils.cpp
@@ -80,7 +80,7 @@ namespace Utils {
             lastTx = millis() - lastTxTime;
             uint32_t statusTx = millis() - statusTime;
             if (statusTx > 10 * 60 * 1000 && lastTx > 10 * 1000) {
-                LoRa_Utils::sendNewPacket(APRSPacketLib::generateStatusPacket(currentBeacon->callsign, "APLRT1", Config.path, "https://github.com/richonguzman/LoRa_APRS_Tracker " + versionDate));
+                LoRa_Utils::sendNewPacket(APRSPacketLib::generateStatusPacket(currentBeacon->callsign, "APLRT1", Config.path, "https://github.com/DJosueMM/LoRa_APRS_Tracker" + versionDate));
                 statusState = false;
                 lastTxTime = millis();
             }
